10ecbce2b759a218878463ef01e5506782336721
Caskia <liubai61@163.com>
2016-12-20 19:15:06 +0800

    push

--- app/components/EventCard.js
+++ app/components/EventCard.js
@@ -1,13 +1,43 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, Button, Image, WebView } from 'react-native';
+import { View, Text, Button, Image, WebView, Animated, Easing } from 'react-native';
 
 export default class EventCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            rotateValue: new Animated.Value(0)
+        };
+    }
+    startAnimation() {
+        Animated.sequence([
+            Animated.timing(this.state.rotateValue, {
+                toValue: 1,
+                duration: 400,
+                easing: Easing.linear
+            }),
+            Animated.timing(this.state.rotateValue, {
+                toValue: 0,
+                duration: 800,
+                easing: Easing.linear
+            })
+        ]).start(() => this.startAnimation());
     }
     render() {
         return (
-            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 30, marginRight: 30 }}>
+            <Animated.View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                marginLeft: 30,
+                marginRight: 30,
+                transform: [{
+                    rotateZ: this.state.rotateValue.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '360deg'] })
+                }]
+            }}>
+                <View>
+                    <Button title='detail' onPress={() => this.startAnimation()} />
+                </View>
                 <View>
                     <Image style={{ height: 140, width: 315 }}
                         source={{ uri: this.props.imageUrl }} />
@@ -23,7 +53,7 @@ export default class EventCard extends Component {
                         scrollEnabled={true}
                         javaScriptEnabled={false} />
                 </View>
-            </View>
+            </Animated.View>
         );
     }
 }
